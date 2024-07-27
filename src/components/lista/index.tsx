ccf48177f6af9d87a6a8a667124c0621c88a39ae
Edilson Tarcio <tarcio.ecp@gmail.com>
2024-07-27 16:15:37 -0300

    Criando function component

---
+++ src/components/lista/index.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+function Lista() {
+  const tarefas = [
+    { tarefa: "React", tempo: "02:00:00" },
+    { tarefa: "Javascript", tempo: "01:00:00" },
+    { tarefa: "Laravel", tempo: "02:00:00" },
+  ];
+  return (
+    <div>
+      {tarefas.map((item, index) => (
+        <div key={index}>
+          <h3> {item.tarefa} </h3>
+          <span> {item.tempo} </span>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default Lista;
